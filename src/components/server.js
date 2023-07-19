0851610754c24813bc3385c3294a84a0c8d6fccc
juancruzlegarra <legarrajuan@gmail.com>
2023-07-19 01:05:25 -0300

    server

---
+++ src/components/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import ProductManager from "./components/ProductManager.js";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const productos = new ProductManager();
+const readProducts = productos.readProducts();
+
+//console.log(await readProducts);
+
+app.get("/products", async (req, res) => {
+  let limit = parseInt(req.query.limit);
+  if (!limit) {
+    return res.send(await readProducts);
+  }
+  let todos = await readProducts;
+  let productLimit = todos.slice(0, limit);
+  res.send(productLimit);
+});
+
+app.get("/products/:id", async (req, res) => {
+  let id = parseInt(req.params.id);
+  let todos = await readProducts;
+  let productsById = todos.find((eL) => eL.id === id);
+  res.send(productsById);
+});
+
+const PORT = 8080;
+const server = app.listen(PORT, () => {
+  console.log(`Express por Localhost ${server.address().port}`);
+});
+
+server.on("error", (error) => {
+  console.log(`error del servidor ${error}`);
+});
