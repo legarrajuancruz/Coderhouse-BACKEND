de3e1bead593272bc77502c946038c6e67c9935f
juancruzlegarra <legarrajuan@gmail.com>
2023-07-24 22:24:06 -0300

    Primer Commit Rama desafio 4

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server port on ${PORT}`);
+});
